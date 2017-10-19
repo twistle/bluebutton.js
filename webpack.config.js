de8332444cc673b1a221a6efd8fadc6e51845719
Lucas Obendorf <lucas.obendorf@gmail.com>
2017-10-19 17:10:16 -0600

    Fix imports in bluebutton.js

--- webpack.config.js
+++ webpack.config.js
@@ -3,15 +3,13 @@ path                    = require('path')
 nodeExternals           = require('webpack-node-externals'),
 FixDefaultImportPlugin  = require('webpack-fix-default-import-plugin');
 
-//let excludes = nodeExternals();
-
 module.exports = {
     entry: {
         'bluebutton': "./lib/bluebutton.js"
     },
     target: "node",
     devtool: 'source-map',
-    //externals: [excludes],
+    externals: [nodeExternals()],
     output: {
         path: path.resolve(__dirname, "build"),
         filename: "[name].js",
